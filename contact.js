f0dacde6861e9ae6bf5e0c5b0da84fd97ebf248a
Ashutosh Tripathi <116786642+999Ashutosh999@users.noreply.github.com>
2024-01-01 14:05:53 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,17 @@
+
+// var temp=[];
+// function formDetails(){
+//     var first_name= document.getElementById("f_name").value;
+//     var last_name= document.getElementById("l_name").value;
+//     var email= document.getElementById("email").value;
+//     var subject=  document.getElementById("subject").value;
+//     var message= document.getElementById("message").value;
+//     var temp_details=[first_name,last_name,email,subject,message]
+//     // contact_details.(temp_details);
+//     // console.log("name:- ",first_name);
+//     var contact_details=JSON.stringify(temp_details);
+//     temp.push(contact_details);
+//     localStorage.setItem("details",contact_details);
+// }
+// console.log(temp);
+// formDetails();
